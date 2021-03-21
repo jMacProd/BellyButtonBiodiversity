7f92c700eddcb3c616dc09b2e6223b856bc1d87d
Jay <j.sutton@vic.chariot.net.au>
2021-03-21 15:56:05 +1100

    Backup app.js - Worked out how to isolate first ten values in a Samples object - not yet dynamic

--- static/js/app.js
+++ static/js/app.js
@@ -39,7 +39,7 @@ function idarray() {
     console.log(samplelist)
     var idlist = []
     samplelist.forEach(function(user) {idlist.push(user.id);});
-    console.log("Array if IDs")
+    console.log("Array of IDs")
     console.log(idlist)
 
     //Create options for dropdown list
@@ -79,14 +79,94 @@ function runEnter() {
   // Prevent the page from refreshing
   d3.event.preventDefault();
 
-  console.log('Filters:')
-  console.log('--------')
+  //console.log('Filter:')
+  
   
   // Select the input element and get the raw HTML node
   var inputElement = d3.select("#selDataset");
   var inputValue = inputElement.property("value");
-  console.log(inputValue);
+  console.log(`Filter: ${inputValue}`);
+  console.log('--------')
+
+  //RUN SOME SORT OF FUNCTION WITH THE INPUT VALUE
 }
+
+function bararrys() {
+
+  d3.json(url).then(function(data) {
+    //create array of sample_values
+    var samplelist = data.samples;
+    console.log("ANOTHER Isolating 'samples' array")
+    console.log(samplelist)
+
+    //Need to pick one object in Samples array
+    // if (samplelist[0].id === '940') {
+    //   console.log(samplelist[0])
+    //   return(samplelist[0]);
+    // }
+    // else(
+    //   console.log('Not equal')
+    //)
+    
+    //get first ten values of sample value
+    //get first ten values of otu_ids
+    //get first ten values ofotu_labels
+    if (samplelist[0].id === '940') {
+      var idnumber = samplelist[0].id;
+      console.log(`ID number: ${idnumber}`);
+
+      var otuids = samplelist[0].otu_ids;
+      console.log('OTU IDs');
+      console.log(otuids);
+
+      var samplevalues = samplelist[0].sample_values;
+      console.log('Sample Values');
+      console.log(samplevalues);
+
+      var otulables = samplelist[0].otu_labels;
+      console.log('OTU LABELS');
+      console.log(otulables);
+      //return(samplelist[0]);
+    }
+    else{
+      console.log('Not equal');
+    }
+
+    otuidsten = otuids.slice(0,10)
+    console.log('OTU Ids Top Ten')
+    console.log(otuidsten)
+
+    samplevaluesten = samplevalues.slice(0,10)
+    console.log('Sample Values Top Ten')
+    console.log(samplevaluesten)
+
+    otulablesten = otulables.slice(0,10)
+    console.log('OTU Labels Top Ten')
+    console.log(otulablesten)
+
+    // var samplevaluelist = []
+    // samplelist.forEach(function(user) {samplevaluelist.push(user.sample_values);});
+    // console.log("Array of all sample values")
+    // console.log(samplevaluelist)
+
+    // //pick one array
+    // One
+
+    
+    
+    
+    
+    // samplevalueten = samplevaluelist.slice(0,10);
+    // console.log("Top ten sample values")
+    // console.log(samplevalueten)
+
+
+
+   
+  });
+} 
+bararrys()
+
   
 //Create a horizontal bar chart with a dropdown menu to display
 //the top 10 OTUs found in that individual.
